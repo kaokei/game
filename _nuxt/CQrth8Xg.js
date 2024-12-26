22bee53fec4f57a8aab8846871bcfd9ecb657823
kaokei <kaokei.rst@gmail.com>
2024-12-27 00:20:45 +0800

    Deploying to gh-pages from @ kaokei/game@8d50f82c4e4c358934063c3a8922e05b70039016 🚀

---
+++ _nuxt/CQrth8Xg.js
@@ -0,0 +1,1 @@
+import{u as t}from"./C0kiXomr.js";import{h as s,c as a,a as o,o as r}from"./DiYP-1BW.js";import"./BkBlkAeX.js";const n={class:"prose ps-6 pt-10 lg:prose-xl"},u=s({__name:"index",setup(p){return t({title:"kaokei"}),(l,e)=>(r(),a("div",n,e[0]||(e[0]=[o("h1",null,"小游戏集合",-1),o("p",null,"积累一些有趣的小游戏。",-1)])))}});export{u as default};
